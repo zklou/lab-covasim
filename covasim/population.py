b5d7d73746f834cdf8700603aaa6020e41b72ad9
zklou <41825013+zklou@users.noreply.github.com>
2021-07-24 17:46:48 -0400

    Apply aggregation to covasim-population.py
    Introduction
    apply aggregation in the project twice.
    
    Abstract
    Found those parts can apply aggregation to simplify code to understand. This is only being used as a practice of apply aggregate in the project which is complex. 
    
    How were the changes made?
    I created three new classes in population.py: "make_people_info", "pop_type", by changing and recode those variables, it made the project easy to add more parameters and further change. 
    
    Why were the changes made?
    While covid is spread, we need to consider a variety of variables about covid, but if the program is hard to change it will make the model less accurate by time passing.

--- covasim/population.py
+++ covasim/population.py
@@ -63,9 +63,10 @@ def make_people(sim, popdict=None, save_pop=False, popfile=None, die=True, reset
             print(f'Warning: not setting ages or contacts for "{location}" since synthpops contacts are pre-generated')
 
     # Actually create the population
-    if sim.people and not reset:
-        return sim.people # If it's already there, just return
-    elif sim.popdict and not reset:
+     if no reset:
+       for sim.people  # If it's already there, just return
+        return sim.people
+       for sim.popdict
         popdict = sim.popdict # Use stored one
         sim.popdict = None # Once loaded, remove
     elif popdict is None: # Main use case: no popdict is supplied
@@ -80,24 +81,6 @@ def make_people(sim, popdict=None, save_pop=False, popfile=None, die=True, reset
         else: # pragma: no cover
             errormsg = f'Population type "{pop_type}" not found; choices are random, clustered, hybrid, or synthpops'
             raise ValueError(errormsg)
-
-     if no reset:
-           for sim.people 
-           return 
-           for sim.popdict
-           si
-     elif popdict is None:
-           
-
-
-
-
-
-
-
-
-
-
 
     # Ensure prognoses are set
     if sim['prognoses'] is None:
@@ -169,8 +152,21 @@ def make_randpop(sim, use_age_data=True, use_household_data=True, sex_ratio=0.5,
             except ValueError as E:
                 if sim['verbose']>=2: # These don't exist for many locations, so skip the warning by default
                     print(f'Could not load household size data for requested location "{location}" ({str(E)}), using default')
-           
-           
+
+    # Actually create the contacts
+    if   microstructure == 'random':    contacts, layer_keys    = make_random_contacts(pop_size, sim['contacts'])
+    elif microstructure == 'clustered': contacts, layer_keys, _ = make_microstructured_contacts(pop_size, sim['contacts'])
+    elif microstructure == 'hybrid':    contacts, layer_keys, _ = make_hybrid_contacts(pop_size, ages, sim['contacts'])
+    else: # pragma: no cover
+        errormsg = f'Microstructure type "{microstructure}" not found; choices are random, clustered, or hybrid'
+        raise NotImplementedError(errormsg)
+
+    popdict['contacts']   = contacts
+    popdict['layer_keys'] = layer_keys
+
+    return popdict
+
+def make_people_info(popdict)       
     # Handle sexes and ages
     uids           = np.arange(pop_size, dtype=cvd.default_int)
     sexes          = np.random.binomial(1, sex_ratio, pop_size)
@@ -187,18 +183,7 @@ def make_randpop(sim, use_age_data=True, use_household_data=True, sex_ratio=0.5,
     popdict['uid'] = uids
     popdict['age'] = ages
     popdict['sex'] = sexes
-
-    # Actually create the contacts
-    if   microstructure == 'random':    contacts, layer_keys    = make_random_contacts(pop_size, sim['contacts'])
-    elif microstructure == 'clustered': contacts, layer_keys, _ = make_microstructured_contacts(pop_size, sim['contacts'])
-    elif microstructure == 'hybrid':    contacts, layer_keys, _ = make_hybrid_contacts(pop_size, ages, sim['contacts'])
-    else: # pragma: no cover
-        errormsg = f'Microstructure type "{microstructure}" not found; choices are random, clustered, or hybrid'
-        raise NotImplementedError(errormsg)
-
-    popdict['contacts']   = contacts
-    popdict['layer_keys'] = layer_keys
-
+                     
     return popdict
 
 
